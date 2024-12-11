82e44adacf6c37a7af22f668c92b13d3e9fc4fd2
Karan Purkait <=>
2024-12-11 22:42:29 +0530

    Normalize line endings

--- init/index.js
+++ init/index.js
@@ -1,18 +1,26 @@
 const mongoose = require("mongoose");
-const initData = require("./data.js");
 const Listing = require("../models/listing.js");
-main().then(()=>{
-    console.log("Connected to DB");
-}).catch(err => console.log(err));
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/yolostay');
-
+  await mongoose.connect(MONGO_URL);
 }
 
-initDB =async()=>{
-   
-    await Listing.insertMany(initData.data);
-    console.log("Data is initialized")
+const initDB = async () => {
+  await Listing.deleteMany({});
+  await Listing.insertMany(require("./data.js").data);
+  console.log("data was initialized");
 };
+
 initDB();
+
+module.exports = initDB;
--- models/listing.js
+++ models/listing.js
@@ -1,27 +1,19 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 
-const listingSchema=new Schema({
-title: {
-    type:String,
-    required:true
-
-},
-description:String,
-image:{
-    filename: { type: String,
-        
-    },
-    url: {
-      type: String,
-      default: 'https://unsplash.com/photos/a-couple-of-houses-sitting-on-top-of-a-lush-green-hillside-0yzmwbDu228',
-    },
-},
-price:Number,
-location:String,
-country:String,
-
+const listingSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  price: Number,
+  location: String,
+  country: String,
+  // Update image field to be an object containing filename and url
+  image: {
+    filename: String,
+    url: String,
+  },
 });
 
-const Listing = mongoose.model("Listing",listingSchema);
-module.exports=Listing;
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
+
